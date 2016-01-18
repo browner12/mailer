2435bd90951c98a4da04bc1fbcb1f975c7603ca9
Andrew <browner12@gmail.com>
2016-01-17 19:30:02 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# mailer
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Build Status][ico-travis]][link-travis]
+[![Coverage Status][ico-scrutinizer]][link-scrutinizer]
+[![Quality Score][ico-code-quality]][link-code-quality]
+[![Total Downloads][ico-downloads]][link-downloads]
+
+This is where your description should go. Try and limit it to a paragraph or two, and maybe throw in a mention of what
+PSRs you support to avoid any confusion with users and contributors.
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require browner12/mailer
+```
+
+## Setup
+
+Generate a mailer using the artisan command. A good practice is to group models together. For example, a mailer for users and a mailer for events.
+
+``` sh
+php artisan make:mailer UserMailer
+```
+
+Create a method within your mailer. The following method will be used to send a confirmation email when a user signs up.
+
+``` php
+method signup($user)
+{
+    $this->name = $user->name;
+    $this->email = $user->email;
+    $this->subject = 'Signup Confirmation';
+    $this->data = [
+        'user' => $user,
+    ];
+}
+```
+
+## Usage
+
+To send a signup email, we will instantiate our `UserMailer`.
+Then call the specific method of the email you would like to send.
+Finally, you can use either the `send()` or `view()` method, depending on if you are testing or actually sending emails.
+
+``` php
+$mailer  = new UserMailer();
+$mailer->signup()->send();
+```
+
+## Change log
+
+Please see [CHANGELOG](CHANGELOG.md) for more information what has changed recently.
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) and [CONDUCT](CONDUCT.md) for details.
+
+## Security
+
+If you discover any security related issues, please email browner12@gmail.com instead of using the issue tracker.
+
+## Credits
+
+- [Andrew Brown][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/browner12/mailer.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/browner12/mailer/master.svg?style=flat-square
+[ico-scrutinizer]: https://img.shields.io/scrutinizer/coverage/g/browner12/mailer.svg?style=flat-square
+[ico-code-quality]: https://img.shields.io/scrutinizer/g/browner12/mailer.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/browner12/mailer.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/browner12/mailer
+[link-travis]: https://travis-ci.org/browner12/mailer
+[link-scrutinizer]: https://scrutinizer-ci.com/g/browner12/mailer/code-structure
+[link-code-quality]: https://scrutinizer-ci.com/g/browner12/mailer
+[link-downloads]: https://packagist.org/packages/browner12/mailer
+[link-author]: https://github.com/browner12
+[link-contributors]: ../../contributors
---
+++ src/Mailer.php
@@ -0,0 +1,150 @@
+<?php namespace browner12\mailer;
+
+use Illuminate\Contracts\Mail\Mailer as MailerContract;
+
+abstract class Mailer
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $email = '';
+
+    /**
+     * @var \App\Models\User
+     */
+    protected $user;
+
+    /**
+     * @var string
+     */
+    protected $subject;
+
+    /**
+     * @var string
+     */
+    protected $view;
+
+    /**
+     * @var array
+     */
+    protected $defaultData = [];
+
+    /**
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * @var array
+     */
+    protected $attachments = [];
+
+    /**
+     * @var \Illuminate\Contracts\Mail\Mailer
+     */
+    private $mailer;
+
+    /**
+     * constructor
+     *
+     * @param \Illuminate\Contracts\Mail\Mailer $mailer
+     */
+    public function __construct(MailerContract $mailer)
+    {
+        //assign
+        $this->mailer = $mailer;
+
+        //global variables for email
+        $this->defaultData = [
+            '_name'  => config('site.name'),
+            '_email' => config('site.email'),
+            '_phone' => config('site.phone'),
+        ];
+    }
+
+    /**
+     * send email
+     *
+     * @param bool $queue
+     * @return bool
+     */
+    final public function send($queue = true)
+    {
+        //php cannot 'use' this variables yet
+        $name = $this->name;
+        $email = $this->email;
+        $subject = $this->subject;
+        $attachments = $this->attachments;
+
+        //data
+        $data = array_merge($this->defaultData, $this->data);
+
+        //queue email
+        if ($queue) {
+
+            $this->mailer->queueOn('email', $this->view, $data,
+                function ($message) use ($name, $email, $subject, $attachments) {
+
+                    //set recipient
+                    $message->to($email, $name);
+
+                    //set subject
+                    $message->subject($subject);
+
+                    //set attachments
+                    foreach ($attachments as $attachment) {
+                        $message->attach($attachment);
+                    }
+                });
+        }
+
+        //send email
+        else {
+
+            $this->mailer->send($this->view, $data,
+                function ($message) use ($name, $email, $subject, $attachments) {
+
+                    //set recipient
+                    $message->to($email, $name);
+
+                    //set subject
+                    $message->subject($subject);
+
+                    //set attachments
+                    foreach ($attachments as $attachment) {
+                        $message->attach($attachment);
+                    }
+                });
+        }
+
+        //make sure to clear out recipient info, so we don't accidentally send multiple times to same person
+        unset($this->name, $this->email, $this->user);
+    }
+
+    /**
+     * view the email
+     *
+     * @return string
+     */
+    final public function view()
+    {
+        //merge data
+        $data = array_merge($this->defaultData, $this->data);
+
+        //setup output
+        $output = '<fieldset style="border-radius:5px; width:95%; margin:5px auto 5px; "><legend>';
+        $output .= $this->subject;
+        $output .= '</legend>';
+        $output .= view($this->view, $data)->render();
+        $output .= '</fieldset>';
+
+        //return
+        return $output;
+    }
+
+}

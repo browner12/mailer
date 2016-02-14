8a962739d77e80d6bc82dad3986e33f6d3f85530
Andrew <browner12@gmail.com>
2016-02-14 15:56:48 -0600

    move command into `Commands` directory
    also this was originally just a copy from some laravel generator.
    adapt it to the mailer.

---
+++ src/Commands/MailerMakeCommand.php
@@ -0,0 +1,48 @@
+<?php namespace browner12\mailer\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MailerMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:mailer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new mailer class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Mailer';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/console.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Mailer';
+    }
+}

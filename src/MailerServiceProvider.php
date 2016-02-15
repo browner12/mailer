6b7ea82dda955e55c876c620418efb91fc2457ef
Andrew <browner12@gmail.com>
2016-02-14 20:35:28 -0600

    create mailer service provider
    right now it is just publishing the config file

---
+++ src/MailerServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace browner12\mailer;
+
+use Illuminate\Support\ServiceProvider;
+
+class MailerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Boot the service provider
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/mailer.php' => config_path('mailer.php')
+        ], 'config');
+    }
+}

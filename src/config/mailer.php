0a8bcb14e429c58bce22252e0217da44f738964c
Andrew <browner12@gmail.com>
2016-02-14 20:34:58 -0600

    create mailer config file

---
+++ src/config/mailer.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Data
+    |--------------------------------------------------------------------------
+    |
+    | If you have data that will be used in every email (possibly in your
+    | templates) this is a good place to set that data once, so each
+    | mailer does not need to set it itself.
+    |
+    */
+    'default_data' => [
+        'company' => 'Acme Co',
+        'email'   => 'info@acme.com',
+        'phone'   => '555-123-4567',
+    ],
+
+];
